f563ccbd5841d0fe0698c81b82fb6c695ced2dcb
jsn-lps <lpsjsn94@gmail.com>
2022-09-19 11:06:28 -0400

    init

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div id="page-wrapper">
+
+      <header>
         <nav id="nav-bar">
           <div id="nav-items-left">
             Howdy world :)
             </div>
 
             <ul id="nav-items-right">
-              <li><a href="#page-wrapper" targe="_blank">Home</a></li>
+              <li><a href="#home-section" targe="_blank">Home</a></li>
               <li ><a href="#about-section">About</a></li>
               <li ><a href="#skills-section">Skills</a></li>
               <li ><a href="#portfolio-section">Portfolio</a></li>
@@ -18,23 +19,33 @@ function App() {
 
             </ul>
           </nav>
+      </header>
 
-        <section id="page-banner">
-          <div id="page-banner-bg1"></div>
-          <div id="page-banner-bg2"></div>
-          <img id="profile-picture" src="https://cdn.drawception.com/drawings/fvtBh78Xbg.png" alt="beautiful person who works hard and does well" />
-        </section>
 
-        <section id="home-section"></section>
+      <div id="page-wrapper">
 
-        <section id="about-section"></section>
 
-        <section id="skills-section"></section>
+          <section id="home-section">
+            <div id="banner"></div>
+            <div id="pfp-box">
+              <div id="page-banner-bg1"></div>
+              <div id="page-banner-bg2"></div>
+              <img id="profile-picture" src="https://cdn.drawception.com/drawings/fvtBh78Xbg.png" alt="beautiful person who works hard and does well" />
+            </div>
+          </section>
 
-        <section id="portfolio-section"></section>
+          <section id="about-section">howdy</section>
 
-        <section id="contact-section"></section>
+          <section id="skills-section"></section>
+
+          <section id="portfolio-section"></section>
+
+          <section id="contact-section"></section>
+
+
       </div>
+
+
     </div>
   );
 }
