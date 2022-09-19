460144e8416092f8e7170e62d5a7d55df30dc469
jsn-lps <lpsjsn94@gmail.com>
2022-09-18 21:27:22 -0400

    framework progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Howdy world :) testing this
+      <div id="page-wrapper">
+        <nav id="nav-bar">Howdy world :)</nav>
+
+        <section id="page-banner">
+          <div id="page-banner-bg1"></div>
+          <div id="page-banner-bg2"></div>
+          <img id="profile-picture" src="https://cdn.drawception.com/drawings/fvtBh78Xbg.png" alt="beautiful person who works hard and does well" />
+        </section>
+
+        <section id="home-section"></section>
+
+        <section id="about-section"></section>
+
+        <section id="skills-section"></section>
+
+        <section id="portfolio-section"></section>
+
+        <section id="contact-section"></section>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
